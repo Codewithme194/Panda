3db45c5616092713770123c4ee59864fe9e86b47
Cezary Szwalbe <P2446634@my365.dmu.ac.uk>
2020-03-26 07:51:36 +0000

    asd

---
+++ Code/PandaBook/Controllers/BookController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PandaBook.Controllers
+{
+    public class BookController : Controller
+    {
+        // GET: Bookhome
+        public string Index()
+        {
+            return "book index";
+        }
+
+
+        public string Product(int id)
+        {
+            return "Product: Id = " + id.ToString();
+        }
+    }
+
+   
+
+
+}
